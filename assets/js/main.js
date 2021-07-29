443314ac4c0dd8933d8ac22e481c105b78ea2680
sabbir-shuvo-ux <79780278+sabbir-shuvo-ux@users.noreply.github.com>
2021-07-29 12:10:06 +0600

    ecom version 0.1

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function() {
+
+$('#offer_big .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+
+});
