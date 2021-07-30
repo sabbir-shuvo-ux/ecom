190bd6b38048f07f335f88050100e5b967a9a1cc
sabbir-shuvo-ux <79780278+sabbir-shuvo-ux@users.noreply.github.com>
2021-07-30 19:25:09 +0600

    this is last time update

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,5 +19,23 @@ $('#offer_big .owl-carousel').owlCarousel({
     }
 })
 
+$('#footer_top .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    responsive:{
+        0:{
+            items:4
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:4
+        }
+    }
+})
 
 });
